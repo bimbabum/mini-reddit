91ffaf7ce0a96ab8537800a2609265db9fb5d2c7
JM <jane.trangm@gmail.com>
2021-12-29 10:36:00 +0700

    Add some basic styling

--- src/features/search/SearchBar.js
+++ src/features/search/SearchBar.js
@@ -2,6 +2,7 @@ import './searchBar.css'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { setSearchTerm } from './searchBarSlice'
+import { loadSearchResults } from '../posts/PostsSlice'
 
 export default function SearchBar(){
     const [term, setTerm] = React.useState('')
@@ -9,16 +10,18 @@ export default function SearchBar(){
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        dispatch(setSearchTerm(term))
+        // dispatch(loadSearchResults(term))
+        // dispatch(setSearchTerm(term))
     }
     return (
-        <form id='searchBar' onSubmit={(e)=>handleSubmit(e)}>
+        // eslint-disable-next-line jsx-a11y/no-redundant-roles
+        <form id='searchBar' role='form' onSubmit={(e)=>handleSubmit(e)}>
             <img src='./img/search_icon.svg' alt='search icon' id='search-icon'/>
             <input
                 placeholder='Search Reddit'
                 id='search'
                 type='text'
-                vale={term}
+                value={term}
                 onChange={(e)=> setTerm(e.target.value)}
             />
         </form>
